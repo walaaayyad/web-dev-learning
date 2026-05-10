878a8d03011fb4eaa2ed325411bf26744accaf68
Walaa Ayyad <walaa_ayyad7@yahoo.com>
2026-05-10 16:43:41 +0300

    upload init files

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <h1 className="text-3xl font-bold">Hello, Next.js!</h1>
+    </div>
+  );
+}
